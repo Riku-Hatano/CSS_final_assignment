c9c27bf9c391e03969f0ab5e96b7f92b30c393df
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-11-25 13:00:39 -0800

    contact: html is done

---
+++ contact.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>contact</title>
+</head>
+<body>
+  <header>
+    <video src="/video/video2.mp4" autoplay loop muted></video>
+    <article>
+      <blockquote>
+        <h1>Cryptocurrencies</h1>
+        <q>The currency of the future</q>
+      </blockquote>
+      <a href="#">Learn more</a>
+    </article>
+  </header>
+  <main>
+    <section class="definition" id="definition">
+      <h2>More info of Definition</h2>
+      <p>
+        Cryptocurrency is only dealt as electric data, and it doesn't have compulsion supported by nation (Technically, El Salvador adopted Bitcoin as a legal tender.)
+      </p>
+      <section>
+        <h3>definition from Wikipedia</h3>
+        <article>
+          <blockquote>
+            1.The system does not require a central authority: its state is maintained through distributed consensus.
+          </blockquote>
+          <blockquote>
+            2.The system keeps an overview of cryptocurrency units and their ownership.
+          </blockquote>
+          <blockquote>
+            3.The system defines whether new cryptocurrency units can be created. If new cryptocurrnecy units can be created, the system defines teh circumstances of tehir origin and how to determine the ownership of these new units.
+          </blockquote>
+          <blockquote>
+            4.Ownership of cryptocurrency units can be proved exclusively cryptographically.
+          </blockquote>
+          <blockquote>
+            5.The system allows transactions to be performed in which ownership of the cryptographic units is changed. A transaction statement can only be issued by an entity proving the current ownership of these units.
+          </blockquote>
+          <blockquote>
+            6.If two different instructions for changing the ownership of the same cryptographic units are simultaneously entered, the system performs at most one of them.
+          </pblockquote
+        </article>
+      </section>
+    </section>
+    <section class="history" id="history">
+      <h2>More info for History</h2>
+      <section>
+        <article>
+          <h3>The first cryptocurrency in the world</h3>
+          <p>
+            On October 2008, Satoshi-Nakamoto published the paper tittled "the settlement system without central administrator by blockchain". Then, He made a first curryptocurrency called Bitcoin with some developper who agreed with his paper. In 2009, Bitcoin started being operated.  
+          </p>
+        </article>
+        <article>
+          <h3>Market of cryptocurrency</h3>
+          <p>
+            On March 2010, "bitcoinmarket.com" appeared as an exchange to deal with curyptocurrency. In the same year, Mt.Gox also appeared. 
+          </p>
+          <p>
+            From 2011 to 2013, Bitcoin became a same value as US dollar, and by on May 2013, other 10 curyptocurrency appeared as rival of Bitcoin. 
+          </p>
+        </article>
+        <article>
+          <h3>Mt.Gox</h3>
+          <p>
+            The more the value of Bitcoin rised, there were more the possibility of cracking. On June 2011, Mt.Gox got cracked for the first time and lost 2000 BTC worth to 3000 US dollars. 
+          </p>
+          <p>
+            Mt.Gox became the largest exchange in 2013, and at maximum, it dealt with 70% of Bitcoin transactions at once. 
+          </p>
+          <p>
+            But, in 2014, Mt.Gox got cracked again and lost 850,000 BTC worth to 460,000,000 US dollars. It was the largest damage of Bitcoin in the history. 
+          </p>
+          <p>
+            After this tragedy, the value of Bitcoin became 50%, and vlaue did not recover by 2016. However, since Mt.Gox got serious cracking, there were no large incidents in cryptocurrency.
+          </p>
+          <p>
+            If you would like to read more, there are links for
+            <a href="https://www.cryptovantage.com/guides/a-brief-history-of-cryptocurrency/" target="_blank">CryptoVangate </a>
+            and 
+            <a href="https://www.forbes.com/sites/bernardmarr/2017/12/06/a-short-history-of-bitcoin-and-crypto-currency-everyone-should-read/?sh=bea1be33f279" target="_blank">Forbs</a>
+          </p>
+        </article>
+      </section>
+    </section>
+    <section class="architecture" id="architecture">
+      <h2>More info of Architecture</h2>
+      <section>
+        <article>
+          <h3>Blockchain</h3>
+          <p>
+            We can say Blockchain is one kind of database. But the largest difference is there is no central administrator. It means we can deal with data without thirdparty. 
+          </p>
+          <p>
+            Comparing to conventional database, it is much difficult to cracking. Because every devices has the same data respectively. We can easily found ilegal canged data by comparing to data in some device to another. That's why, this tecnology is good for dealing with currency.
+          </p>
+        </article>
+        <article>
+          <h3>Mining</h3>
+          <p>
+            Mining is getting cryptocurrency as reward.
+          </p>
+          <p>
+            Every fixed period, cryptocurrency appends a history of transaction. But because of the amount of data, it has to be helped by someone. 
+          </p>
+          <p>
+            When we finish to append, we can take cryptocurrency. At this moment, new crypt was issued. This is called "Mining". In other words, if there was no one to help this task, that kind of cryptocurrency get broken. 
+          </p>
+        </article>
+        <article>
+          <h3>Node</h3>
+          <p>
+            As main page mentioned, node is a devece that is joining in the network of cryptocurrency. 
+          </p>
+          <p>
+            In the system, node has a huge role. Each node is monitoring each other. If there were some difference between one and another, there may be some cracking.
+          </p>
+          <p>
+            It follows that all of devices that dealt with specific cryptocurrency have all of history of transactions. As we can see, it is very difficult to cracking. If you want to, you have to falsify the data in all of devices at once.
+          </p>
+        </article>
+      </section>
+    </section>
+    <section class="legality" id="legality">
+      <h2>More info of Leagality</h2>
+      <article>
+        <h3>US</h3>
+        <p>
+          As homepage mentioned, not all of states of US is legal. On the contrary, only five out of fifty states is legal.  
+        </p>
+        <p>
+          But more states will see is as legal. Because SEC (Securities nad Exchange Comission) regarded cryptocurrencyas as securities and on March 2018, it proclaimed that the security law will be expanded to cryptocurrency. 
+        </p>
+        <p>
+          And next year, 70% of states of US have already set the law related to crytpcurrency and blockchain. In Arizona and Georgia state, we can pay tax in cryptocurrency. 
+        </p>
+      </article>
+      <article>
+        <h3>China</h3>
+        <p>
+          In China, the posture for cryptocurrency is totally strict.
+        </p>
+        <p>
+          Spokes person of People's Bank of China said, Cryptocurrency make order of economy and finance messed up and it is often used as money laundering, illegal fundraising, and deception.
+        </p>
+        <p>
+          What is more, Central committee said if such kind of braking law was detected, it will be imposed criminal penalties. And if overseas companies do same, criminal penalties will be imposed on domestic stakeholders.
+        </p>
+      </article>
+    </section>
+  </main>
+  <footer>
+      <article>
+        <h4>Authors</h4>
+        <aside>
+          <i class="fa-brands fa-github"></i>
+          <span>Riku Hatano</span>
+        </aside>
+        <aside>
+          <i class="fa-brands fa-github"></i>
+          <span>Mauricio Robles</span>
+        </aside>
+      </article>
+      <a href="#">
+        <i class="fa-solid fa-angle-up"></i>
+        top
+      </a>
+  </footer>
+  <?php 
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+  $interest = $_POST["interest"];
+  $json_interest = json_encode($interest);
+
+  echo <<<EOM
+  <script type="text/javascript">
+  let interest = {$json_interest};
+  console.log(definition);
+  for (let i = 0 ; i < interest.length ; i++) {
+    let section = document.getElementById(interest[i])
+    section.style.display = "flex";
+  }
+  </script>;
+  EOM;
+  ?>
+</body>
+</html>
